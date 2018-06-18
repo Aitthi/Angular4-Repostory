e108202b196758a1ccecfa7863de7b4c6ee743b8
Aitthi Arsa <meanstack20@gmail.com>
2018-06-18 11:35:52 +0700

    .

--- src/app/class/repos.list.ts
+++ src/app/class/repos.list.ts
@@ -1,5 +1,7 @@
 export class ReposList{
     reponame = [
-        {name: 'angular/angular'}
+        {name: 'angular/angular'},
+        {name: 'vuejs/vue'},
+        {name: 'facebook/react'}
     ]
 }
